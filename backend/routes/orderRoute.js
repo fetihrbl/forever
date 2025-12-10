af3a099f64161282e97774bfb320bd613eed10b0
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2025-12-10 08:56:10 -0800

    order backend

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, getMyOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
+import { createOrder, getUserOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { adminAuth } from "../middlewares/adminAuth.js";
 
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // USER ROUTES
 router.post("/create", authMiddleware, createOrder);
-router.get("/my-orders", authMiddleware, getMyOrders);
+router.get("/my-orders", authMiddleware, getUserOrders);
 
 // ADMIN ROUTES
 router.get("/all", adminAuth, getAllOrders);
